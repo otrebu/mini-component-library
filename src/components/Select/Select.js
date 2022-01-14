ccd18fbe3e3feef27f68128e0dffead81355e7d6
otrebu <dev@uberto.me>
2022-01-14 14:24:11 +0000

    feat: select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-const Select = ({ label, value, onChange, children }) => {
+const SelectWrapper = styled.div`
+  position: relative;
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const SelectIcon = styled(Icon)`
+  top: 10px;
+  right: 6px;
+  position: absolute;
+  display: inline-block;
+  color: ${COLORS.gray700};
+`;
+
+const Select = styled.select`
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  border: none;
+  padding: 12px 52px 12px 16px;
+  color: ${COLORS.gray700};
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  line-height: 19px;
+
+  &:focus {
+    border: 2px solid #4374cb;
+    border-radius: 3px;
+  }
+`;
+
+const MySelect = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <Select value={value} onChange={onChange}>
+        {children}
+      </Select>
+      <SelectIcon id="chevron-down" />
+    </SelectWrapper>
   );
 };
 
-export default Select;
+export default MySelect;
